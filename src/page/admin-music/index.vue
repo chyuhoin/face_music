925057b6c05ecd91717466e2cb7ef895a863d252
chyuhoin <694546200@qq.com>
2023-12-14 21:08:08 +0800

    音乐管理改进

--- src/page/admin-music/index.vue
+++ src/page/admin-music/index.vue
@@ -1,107 +1,85 @@
 <template>
     <div class="admin-music">
-      <div class="id-input">
-        <span style="margin-right: 50px;">音乐ID</span>
-        <el-input v-model="id" placeholder="请输入音乐id" style="width: 30%;"></el-input>
-      </div>
-      <div class="block">
-        <span>Anger</span>
-        <el-slider v-model="anger" :show-tooltip="false" style="display: inline;"></el-slider>
-      </div>
-      <div class="block">
-        <span>Disgust</span>
-        <el-slider v-model="disgust" :show-tooltip="false"></el-slider>
-      </div>
-      <div class="block">
-        <span>Fear</span>
-        <el-slider v-model="fear" :show-tooltip="false"></el-slider>
-      </div>
-      <div class="block">
-        <span>Happiness</span>
-        <el-slider v-model="happiness" :show-tooltip="false"></el-slider>
-      </div>
-      <div class="block">
-        <span>Neutral</span>
-        <el-slider v-model="neutral" :show-tooltip="false"></el-slider>
-      </div>
-      <div class="block">
-        <span>Sadness</span>
-        <el-slider v-model="sadness" :show-tooltip="false"></el-slider>
-      </div>
-      <div class="block">
-        <span>Surprise</span>
-        <el-slider v-model="surprise" :show-tooltip="false"></el-slider>
-      </div>
-      <el-button @click="submit()">添加音乐</el-button>
+      <AddMusic :opened="openDialog"/>
+      <el-button @click="addMusic()">添加音乐</el-button>
+      <SongTable
+        :songs="songs"
+        header-row-class-name="header-row"
+      />
     </div>
   </template>
   
 <script>
-import { myRequest, notify } from '@/utils'
-import { confirm } from '@/base/confirm'
+import { myRequest, notify, createSong } from '@/utils'
 import { getSongDetail } from '@/api'
+import SongTable from "@/components/song-table"
+import AddMusic from "@/components/add-music"
 
 export default {
   data() {
     return {
-      id: "",
-      anger: 50,
-      disgust: 50,
-      fear: 50,
-      happiness: 50,
-      neutral: 50,
-      sadness: 50,
-      surprise: 50,
+      songs: [],
+      openDialog: false
     }
   },
   methods: {
-    async submit() {
-      if (this.id === "") {
-        confirm("请输入音乐ID！")
-        return;
-      }
-      let total = this.anger + this.disgust + this.fear + this.happiness + this.neutral + this.sadness + this.surprise + 7;
-      let emotion = {
-        anger: (this.anger + 1) / total,
-        disgust: (this.disgust + 1) / total,
-        fear: (this.fear + 1) / total,
-        happiness: (this.happiness + 1) / total,
-        neutral: (this.neutral + 1) / total,
-        sadness: (this.sadness + 1) / total,
-        surprise: (this.surprise + 1) / total,
-      }
-      let detail = await getSongDetail(this.id);
-      if (!detail.songs[0]) {
-        confirm("找不到这样的歌！")
-        return;
-      }
-      let resp = await myRequest.post('/add/music', {id: this.id, name: detail.songs[0].name, emotion: JSON.stringify(emotion), style: ""})
-      if (resp.data.name) {
-        notify.success(`音乐${resp.data.name}添加成功！`)
-        this.id = ""
-      }
+    addMusic() {
+      this.openDialog = true;
+    },
+    async getSongData() {
+      let resp = await myRequest.get('/musicIds');
+      let ids = resp.data.ids;
+      let { songs } = await getSongDetail(ids);
+      this.songs = songs.map(song => {
+        const {
+          id,
+          name,
+          ar: artists,
+          dt: duration,
+          mv,
+          al: { picUrl, name: albumName }
+        } = song
+        return createSong({
+          id,
+          name,
+          artists,
+          duration,
+          albumName,
+          img: picUrl,
+          mvId: mv
+        })
+      })
     }
   },
   mounted() {
+    this.getSongData();
+  },
+  components: {
+    SongTable,
+    AddMusic
   }
 }
 </script>
 
 <style lang="scss" scoped>
 .admin-music {
-  padding: 50px;
-  text-align: center;
+  padding: 12px;
+  max-width: 50%;
+  margin: auto;
+  .header-row {
+    display: none;
+  }
 }
-.id-input {
-  display: inline;
-  margin-bottom: 10px;
-}
-.block {
-  width: 30%;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 40px;
-  display: flexbox;
-}
+// .id-input {
+//   display: inline;
+//   margin-bottom: 10px;
+// }
+// .block {
+//   width: 30%;
+//   margin-left: auto;
+//   margin-right: auto;
+//   margin-top: 40px;
+//   display: flexbox;
+// }
 </style>
   
